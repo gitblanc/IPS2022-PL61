db697b3860eb12932f838cf01f11d3e2168eea2f
Eduardo Blanco Bielsa <blanc@MacBook-Pro-de-Eduardo.local>
2022-10-05 12:14:11 +0200

    actividad,recurso,administrador

---
+++ ips2022-pl61/src/logic/Actividad.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author UO285176
+ *
+ */
+public class Actividad {
+	private boolean plazasLimitadas;
+	private int limitePlazas;
+	private IntensidadActividad intensidad;
+	private List<Recurso> recursos = new ArrayList<>();
+}
---
+++ ips2022-pl61/src/logic/Administrador.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package logic;
+
+/**
+ * @author UO281576
+ *
+ */
+public class Administrador {
+
+	public void crearActividad() {
+		// TODO Auto-generated method stub
+		
+	}
+}
