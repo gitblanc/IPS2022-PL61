f88125c4231eef59255018ee75c2b535a23970ed
Xin <87143166+iimxinn@users.noreply.github.com>
2022-11-26 21:28:26 +0100

    Revert "Hu 18962 (socio)"

--- ips2022-pl61/src/database/business/alquiler/crud/FindByIdSocio.java
+++ ips2022-pl61/src/database/business/alquiler/crud/FindByIdSocio.java
@@ -81,7 +81,6 @@ public class FindByIdSocio {
 		a.hora_fin = r.getString("Hora_fin");
 		a.instalacion = r.getString("Nombre_i");
 		a.fecha = r.getString("fecha");
-		a.cancelado = r.getInt("cancelado");
 
 		return a;
 	}
